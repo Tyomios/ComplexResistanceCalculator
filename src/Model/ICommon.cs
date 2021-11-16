882adfb82c77b979aba88edb25cf183e4dc17d14
Tyomios <tyomios@mail.ru>
2021-11-16 11:14:59 +0700

    Начал работать с логикой

---
+++ src/Model/ICommon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Model
+{
+	/// <summary>
+	/// Тип соединения узла
+	/// </summary>
+	public enum ConnectionType
+	{
+		Parallel,
+		ContinueParallel,
+		Common
+	}
+
+	public interface ICommon
+	{
+		public ConnectionType Type { get; set; }
+
+		abstract List<Complex> CalculateZ(List<double> frequency);
+
+
+	}
+}
